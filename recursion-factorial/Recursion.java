bbb625229b0f8a0997e11cf19471548408d0231e
mthsps <dmatheusps@gmail.com>
2022-05-12 23:07:15 -0300

    Recursion

---
+++ recursion-factorial/Recursion.java
@@ -0,0 +1,16 @@
+public class Recursion {
+
+    public static int factorial(int x) {
+        if (x == 1)
+            return 1;
+        else
+            return x * factorial(x - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial(5));
+        
+    }
+    
+}
